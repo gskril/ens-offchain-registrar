e6b396f10577b78eec03359e1f272ce4f1fc24ad
Greg <gskril@gmail.com>
2023-05-12 23:34:22 -0400

    Allow saving temporary keys

--- worker/src/handlers/functions/set.ts
+++ worker/src/handlers/functions/set.ts
@@ -1,11 +1,16 @@
 import { NameData } from '../../models/data'
 declare const RECORDS: KVNamespace
 
-export async function set(name: string, records: NameData) {
+export async function set(
+  name: string,
+  records: NameData,
+  isTemporary?: boolean
+) {
   const value = JSON.stringify(records)
 
   try {
     await RECORDS.put(name, value, {
+      expirationTtl: isTemporary ? 86_400 : undefined, // Discard demo keys after 24 hours
       metadata: {
         updated_at: new Date().toISOString(),
       },
