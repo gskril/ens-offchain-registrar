69c6ed84a65950ea8ebeb9b7e2ace59cde969241
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-09-09 01:45:32 -0400

    Optimize DNS decoding

--- worker/src/ccip-read/utils.ts
+++ worker/src/ccip-read/utils.ts
@@ -1,10 +1,5 @@
-import { Buffer } from 'buffer'
-import packet from 'dns-packet'
+import { type ByteArray, bytesToString, toBytes } from 'viem'
 import { parseAbi } from 'viem/utils'
-
-export function dnsDecodeName(name: string): string {
-  return packet.name.decode(Buffer.from(name.slice(2), 'hex'))
-}
 
 export const resolverAbi = parseAbi([
   'function resolve(bytes calldata name, bytes calldata data) external view returns(bytes memory result, uint64 expires, bytes memory sig)',
@@ -14,3 +9,27 @@ export const resolverAbi = parseAbi([
   'function text(bytes32, string) external pure returns (string memory)',
   'function contenthash(bytes32) external pure returns (bytes memory)',
 ])
+
+export function dnsDecodeName(encodedName: string): string {
+  const bytesName = toBytes(encodedName)
+  return bytesToPacket(bytesName)
+}
+
+// Taken from ensjs https://github.com/ensdomains/ensjs/blob/main/packages/ensjs/src/utils/hexEncodedName.ts
+function bytesToPacket(bytes: ByteArray): string {
+  let offset = 0
+  let result = ''
+
+  while (offset < bytes.length) {
+    const len = bytes[offset]
+    if (len === 0) {
+      offset += 1
+      break
+    }
+
+    result += `${bytesToString(bytes.subarray(offset + 1, offset + len + 1))}.`
+    offset += len + 1
+  }
+
+  return result.replace(/\.$/, '')
+}
