70f2c369099e8852187cca76cc71871f5669b8e3
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2026-08-10 17:50:11 -0400

    Update link

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -3,10 +3,7 @@ import { Link } from '@/components/ui'
 export function Footer() {
   return (
     <footer className="flex gap-6">
-      <Link href="https://docs.ens.domains/dapp-developer-guide/ens-l2-offchain">
-        Docs
-      </Link>
-
+      <Link href="https://docs.ens.domains/learn/ccip-read/">Docs</Link>
       <Link href="https://github.com/gskril/ens-offchain-registrar">Repo</Link>
     </footer>
   )
